c577bda2065026993b23273bd84d104b4b17f714
LAPTOP-O23QBV2D\ASUS <fadilah.insyani@gmail.com>
2023-05-07 22:48:50 +0800

    membuat folder project laporan 1

---
+++ views/krs-mahasiswa/index.php
@@ -0,0 +1,9 @@
+<?php
+/** @var yii\web\View $this */
+?>
+
+
+<p><h1>krs-mahasiswa/index</h1>
+    You may change the content of this page by modifying
+    the file <code><?= __FILE__; ?></code>.
+</p>
